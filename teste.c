cb74d9b87ecd9b49020956092ed6f8710a16f9c1
Pedro Henrique <pedro.henrique.goncalves0701@gmail.com>
2025-02-16 18:53:50 -0300

    Primeiro teste de arquivo

---
+++ teste.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+#include <math.h>
+
+int main() 
+{
+    printf("Hello World!");
+    printf("Hello World22!");
+    scanf("%d");
+    printf("Hello World33!");
+    return 0;
+ }
